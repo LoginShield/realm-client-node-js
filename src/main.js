1d62a04fbae48cb54f5b31b89b0a1880ab079a1a
Jonathan Buhacoff <jonathan@buhacoff.net>
2020-02-07 20:40:05 -0800

    add: immediate registration method without redirect to loginshield

--- src/main.js
+++ src/main.js
@@ -5,19 +5,81 @@ Copyright (C) 2019 Cryptium Corporation. All rights reserved.
 const ajax = require('axios');
 
 class LoginShield {
-    constructor({endpointURL, realmId, authorizationToken} = {}) {
+    constructor({ endpointURL, realmId, authorizationToken } = {}) {
         this.endpointURL = endpointURL || process.env.LOGINSHIELD_ENDPOINT_URL;
         this.realmId = realmId || process.env.LOGINSHIELD_REALM_ID;
         this.authorizationToken = authorizationToken || process.env.LOGINSHIELD_AUTHORIZATION_TOKEN;
     }
 
     /**
+     * Register a new user with the 'immediate' method.
+     *
+     * The immediate method is preferred for the best user experience.
+     * To use the immediate method, provide `realmScopedUserId`, `name`, and `email`,
+     * and if the service responds with { isCreated: true } then continue to the
+     * first login with LoginShield (specify the new key flag) to complete registration.
+     *
+     * `realmScopedUserId`: how LoginShield should identify the user for this authentication realm
+     * `name`: the user's display name
+     * `email`: the user's email address
+     * @param {*} param0
+     */
+    async createRealmUser({
+        realmScopedUserId, name, email,
+    }) {
+        try {
+            const request = {
+                realmId: this.realmId,
+                realmScopedUserId,
+                name,
+                email,
+            };
+            console.log('createRealmUser request: %o', request);
+            const headers = {
+                Authorization: `Token ${this.authorizationToken}`,
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            };
+            const response = await ajax.post(
+                `${this.endpointURL}/service/realm/user/create`,
+                JSON.stringify(request),
+                {
+                    headers,
+                },
+            );
+            console.log('createRealmUser response status: %o', response.status);
+            console.log('createRealmUser response status text: %o', response.statusText);
+            console.log('createRealmUser response headers: %o', response.headers);
+            console.log('createRealmUser response data: %o', response.data);
+            if (response.data && response.data.isCreated) {
+                return response.data; // { isCreated: true }
+            }
+            return { error: 'unexpected-response', response }; // { isCreated: false, fault }
+        } catch (err) {
+            console.log('createRealmUser error', err);
+            return { error: 'registration-failed', err };
+        }
+    }
+
+    /**
+     * Register a new user with the 'redirect' method.
+     *
+     * The immediate method is preferred for the best user experience.
+     *
+     * To use the redirect method, provide `realmScopedUserId` and `redirect`,
+     * and if the service responds with { isCreated: true, forward: <url> } then
+     * redirect the user to that forward URL; and when the service has registered
+     * the user, the service will redirect the user back to the specified `redirect`
+     * URL.
+     *
      * `realmScopedUserId`: how LoginShield should identify the user for this authentication realm
      * `redirect`: where loginshield will redirect the user after the user authenticates and confirms the link with the realm (the enterprise should complete the registration with the first login with loginshield at this url)
-     * @param {*} param0 
+     * @param {*} param0
      */
-    async createRealmUser({ realmScopedUserId, redirect }) {
-        try {            
+    async createRealmUserWithRedirect({
+        realmScopedUserId, redirect,
+    }) {
+        try {
             const request = {
                 realmId: this.realmId,
                 realmScopedUserId,
@@ -133,7 +195,6 @@ class LoginShield {
             return { error: 'login-failed' };
         }
     }
-
 }
 
 export { LoginShield };
